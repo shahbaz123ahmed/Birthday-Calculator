9a01370d52240aa722b2ca33a7c35a59895d0424
shahbaz123ahmed <shahbaz126ahmed@gmail.com>
2024-09-28 00:31:18 +0530

    sec

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-age-calculator/',
-  plugins: [reactRefresh()],
+  plugins: [react()],
   build: {
-    outDir: './build',
+    outDir: './dist', // Change to './build' if necessary
   },
   optimizeDeps: {
-    include: ["tsparticles"]
-  }
-})
+    include: ['tsparticles'],
+  },
+});
